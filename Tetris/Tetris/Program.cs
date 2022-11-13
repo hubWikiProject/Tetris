bee22f6e2eaa6e6ad714b5addd836b50dca2aea1
hub Wiki Project <response200@vivaldi.net>
2022-11-13 23:07:13 +0700

    #2.Вывод двух точек на экран

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(1112);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.SetWindowSize(40, 30);
+        Console.SetBufferSize(40, 30);
+
+        int x1 = 3;
+        int y1 = 4;
+        char c1 = '*';
+        Draw(x1, y1, c1);
+
+        int x2 = 5;
+        int y2 = 7;
+        char c2 = '#';
+        Draw(x2, y2, c2);
+
+        Console.ReadLine();
+    }
+
+    static void Draw(int x, int y, char c)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(c);
+    }
+}
