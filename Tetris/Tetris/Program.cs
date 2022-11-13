a82758c0bb1565b26de1ea10487c25001d0afedd
hub Wiki Project <stukalov.blog@gmail.com>
2022-11-13 23:29:26 +0700

    #4.Добавлен класс Point

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,26 +1,17 @@
-﻿internal class Program
+﻿using Tetris;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.SetWindowSize(40, 30);
         Console.SetBufferSize(40, 30);
 
-        int x1 = 3;
-        int y1 = 4;
-        char c1 = '*';
-        Draw(x1, y1, c1);
-
-        int x2 = 5;
-        int y2 = 7;
-        char c2 = '#';
-        Draw(x2, y2, c2);
-
+        Point p1 = new Point();
+        p1.x = 3;
+        p1.y = 4;
+        p1.c = '*';
+        p1.Draw();
         Console.ReadLine();
-    }
-
-    static void Draw(int x, int y, char c)
-    {
-        Console.SetCursorPosition(x, y);
-        Console.Write(c);
     }
 }
